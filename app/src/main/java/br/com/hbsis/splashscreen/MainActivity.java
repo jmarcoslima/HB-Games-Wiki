b9a54aa8cc9cb5e092ccdf6c22ba9d138e6b481b
jmarcoslima <jmarcos663@gmail.com>
2019-11-27 11:47:49 -0300

    Commit inicial
    Splash Screen
    Navigation Drawer

--- app/src/main/java/br/com/hbsis/splashscreen/MainActivity.java
+++ app/src/main/java/br/com/hbsis/splashscreen/MainActivity.java
@@ -1,18 +1,38 @@
 package br.com.hbsis.splashscreen;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    RelativeLayout maincontent;
+    LinearLayout mainmenu;
+    Button btnMenu;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Removendo ActionBar
         ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+
+       btnMenu = findViewById(R.id.btnmenu);
+
+       maincontent  = findViewById(R.id.maincontent);
+       mainmenu = findViewById(R.id.mainmenu);
+
+       btnMenu.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               maincontent.animate().translationX(780);
+               mainmenu.animate().translationX(0);
+           }
+       });
     }
 }
